2768e48c2e7f7d4cf333304c5da8dd9433861681
Anton <garynets.anton@gmail.com>
2025-08-04 16:02:24 +0300

    Add initialization of squares

--- src/components/Game/Board/Board.tsx
+++ src/components/Game/Board/Board.tsx
@@ -7,16 +7,39 @@ interface SquareInterface {
     value: Number;
 }
 
+function initializeRandomSquare(arr: SquareInterface[][]) {
+    arr = arr.map((row) => row.map((cell) => ({ ...cell })));
+
+    let [randomIndexI, randomIndexJ] = [
+        Math.floor(Math.random() * 4),
+        Math.floor(Math.random() * 4),
+    ];
+    while (arr[randomIndexI][randomIndexJ].value) {
+        [randomIndexI, randomIndexJ] = [
+            Math.floor(Math.random() * 4),
+            Math.floor(Math.random() * 4),
+        ];
+    }
+
+    arr[randomIndexI][randomIndexJ].value = 2;
+    return arr;
+}
+
 function Board() {
     // create a 4x4 matrix filled with SquareInterface objects
     const [squares, setSquares] = useState(() => {
         let arr: SquareInterface[][] = [];
+
         for (let i = 0; i < 4; i++) {
             arr.push(new Array());
             for (let j = 0; j < 4; j++) {
                 arr[i].push({ id: i * 4 + j, value: 0 } as SquareInterface);
             }
         }
+
+        // initialize two random squares with value of '2'
+        arr = initializeRandomSquare(arr);
+        arr = initializeRandomSquare(arr);
         return arr;
     });
 

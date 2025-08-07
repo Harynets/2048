c29ef68ab7574a877fe499a4b75f81ca0e1f70eb
Anton <garynets.anton@gmail.com>
2025-08-07 15:48:16 +0300

    Add color for squares with value

--- src/components/Game/Board/Square.tsx
+++ src/components/Game/Board/Square.tsx
@@ -2,12 +2,30 @@ import React from "react";
 import "./Square.css";
 
 interface Props {
-    squareValue: Number;
+    squareValue: number;
 }
 
 function Square({ squareValue }: Props) {
+    const squareColor: { [key: number]: string } = {
+        2: "bg-yellow-200",
+        4: "bg-yellow-300",
+        8: "bg-yellow-500",
+        16: "bg-lime-400",
+        32: "bg-lime-600 text-white",
+        64: "bg-green-700 text-white",
+        128: "bg-teal-500 text-white",
+        256: "bg-cyan-500 text-white",
+        512: "bg-sky-600 text-white",
+        1024: "bg-blue-700 text-white",
+        2048: "bg-indigo-800 text-white",
+    };
+
     return (
-        <div className="square flex justify-center items-center bg-amber-50 border-stone-300 border-5">
+        <div
+            className={`square flex justify-center items-center ${
+                squareColor[squareValue] ?? "bg-amber-50"
+            } border-stone-300 border-5`}
+        >
             <h1 className="font-semibold">{squareValue.toString()}</h1>
         </div>
     );

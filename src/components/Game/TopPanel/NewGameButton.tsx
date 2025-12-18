45c25d26e4d73d1714b8ef4d460a24628ac97ccc
Anton <garynets.anton@gmail.com>
2025-12-18 20:49:04 +0200

    Update NewGameButton styles

--- src/components/Game/TopPanel/NewGameButton.tsx
+++ src/components/Game/TopPanel/NewGameButton.tsx
@@ -7,8 +7,9 @@ interface Props {
 function NewGameButton({ startNewGame }: Props) {
     return (
         <button
+            type="button"
             onClick={startNewGame}
-            className="cursor-pointer bg-[#fae48b] border border-stone-300 rounded-lg p-2 mb-3 ms-3 w-full hover:bg-[#f9db65] active:scale-95 focus:outline-none font-bold text-lg shadow-sm"
+            className="bg-[#fae48b] border border-stone-300 lg:hover:bg-[#f9db65] active:scale-99 lg:active:brightness-97 focus:outline-none cursor-pointer font-bold text-lg shadow-sm rounded-lg p-2 mb-3 ms-3 w-full"
         >
             New Game
         </button>
